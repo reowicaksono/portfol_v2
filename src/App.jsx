f4ce92eedcff6f6e18e8bc7d6c61ebd5031ff516
reowicaksono <reo.wicaksono.2005356@gmail.com>
2025-02-05 23:38:36 +0700

    [update] add route certification, port, and experience

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
 import { Route, Routes } from 'react-router';
 import Home from './views/home/HomeView.jsx';
 import About from './views/about/AboutView.jsx';
-
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
+import Portfolio from './views/portfolio/PortfolioView.jsx';
+import Experience from './views/experience/ExperienceView.jsx';
+import Ceritification from './views/certification/CertificationView.jsx';
 
 function App() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000, // Waktu animasi dalam ms
+      once: true, // Animasi hanya muncul sekali saat scroll
+    });
+  }, []);
 
   return (
-   <Routes>
+    <Routes>
       <Route index element={<Home />} />
       <Route path="about" element={<About />} />
-
+      <Route path="experience" element={<Experience />} />
+      <Route path="portfolio" element={<Portfolio />} />
+      <Route path="certification" element={<Ceritification />} />
     </Routes>
-
-  )
+  );
 }
 
-export default App
+export default App;
