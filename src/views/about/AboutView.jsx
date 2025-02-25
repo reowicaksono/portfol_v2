f01b1e47851030b9dec8f118dc0c4866ec738440
reowicaksono <reo.wicaksono.2005356@gmail.com>
2025-02-25 09:41:28 +0700

    remove php stack & add flutter

--- src/views/about/AboutView.jsx
+++ src/views/about/AboutView.jsx
@@ -16,16 +16,14 @@ const AboutView = () => {
   const tech = [
     { id: 1, name: 'HTML', imageUrl: 'https://cdn-icons-png.flaticon.com/512/1051/1051277.png', status: 'Advanced' },
     { id: 2, name: 'CSS', imageUrl: 'https://cdn-icons-png.flaticon.com/512/732/732190.png', status: 'Advanced' },
-    { id: 3, name: 'PHP', imageUrl: 'https://cdn.iconscout.com/icon/free/png-256/free-php-2038871-1720084.png', status: 'Advanced' },
-    { id: 4, name: 'JavaScript', imageUrl: 'https://cdn.icon-icons.com/icons2/2415/PNG/512/javascript_original_logo_icon_146455.png', status: 'Advanced' },
-    { id: 5, name: 'Laravel', imageUrl: 'https://cdn.worldvectorlogo.com/logos/laravel-2.svg', status: 'Advanced' },
-    { id: 6, name: 'CodeIgniter', imageUrl: 'https://cdn-peanutsquare.b-cdn.net/wp-content/uploads/2022/11/codeigniter-logo.png', status: 'Intermediate' },
-    { id: 7, name: 'VueJS', imageUrl: 'https://cdn.iconscout.com/icon/free/png-256/free-vue-282497.png?f=webp', status: 'Intermediate' },
-    { id: 8, name: 'AlpineJS', imageUrl: 'https://avatars.githubusercontent.com/u/59030169?s=200&v=4', status: 'Beginner' },
-    { id: 9, name: 'NodeJS', imageUrl: 'https://cdn.iconscout.com/icon/free/png-256/free-node-js-1174925.png', status: 'Beginner' },
-    { id: 10, name: 'Tailwind', imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Tailwind_CSS_Logo.svg', status: 'Advanced' },
-    { id: 11, name: 'Bootstrap', imageUrl: 'https://getbootstrap.com/docs/5.2/assets/brand/bootstrap-logo-shadow.png', status: 'Intermediate' },
-    { id: 12, name: 'ReactJS', imageUrl: 'https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png', status: 'Beginner' }
+    { id: 3, name: 'JavaScript', imageUrl: 'https://cdn.icon-icons.com/icons2/2415/PNG/512/javascript_original_logo_icon_146455.png', status: 'Advanced' },
+    { id: 4, name: 'VueJS', imageUrl: 'https://cdn.iconscout.com/icon/free/png-256/free-vue-282497.png?f=webp', status: 'Intermediate' },
+    { id: 5, name: 'AlpineJS', imageUrl: 'https://avatars.githubusercontent.com/u/59030169?s=200&v=4', status: 'Beginner' },
+    { id: 6, name: 'NodeJS', imageUrl: 'https://cdn.iconscout.com/icon/free/png-256/free-node-js-1174925.png', status: 'Beginner' },
+    { id: 7, name: 'Tailwind', imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Tailwind_CSS_Logo.svg', status: 'Advanced' },
+    { id: 8, name: 'Bootstrap', imageUrl: 'https://getbootstrap.com/docs/5.2/assets/brand/bootstrap-logo-shadow.png', status: 'Intermediate' },
+    { id: 9, name: 'ReactJS', imageUrl: 'https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png', status: 'Beginner' },
+    { id: 10, name: 'Flutter', imageUrl: 'https://img.icons8.com/?size=100&id=7I3BjCqe9rjG&format=png&color=000000', status: 'Advanced' }
   ];
 
   const tools = [
